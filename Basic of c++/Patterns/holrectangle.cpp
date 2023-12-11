306891848ac47844a7a859296d2ad9ac4b11eb58
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2023-12-11 11:56:51 +0530

    patterns

---
+++ Basic of c++/Patterns/holrectangle.cpp
@@ -0,0 +1,24 @@
+//  ****
+//  *  *
+//  *  *
+//  *  *
+//  ****
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int row,col;
+
+    cin>>row>>col;
+
+    for (int i = 0; i < row; i++)
+    {
+        if(row==0){
+            for
+        }
+    }
+    
+    return 0;
+}
