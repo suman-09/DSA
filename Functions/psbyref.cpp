746c8dcf5969628eecba77e05066908ee4abe75f
Suman Karmakar <sumankarmakar25112002@gmail.com>
2023-12-14 13:27:37 +0530

    pass of ref of array

--- Functions/psbyref.cpp
+++ Functions/psbyref.cpp
@@ -2,14 +2,34 @@
 using namespace std;
 
 // pass by reference
-void dosomething(string &s){ // now it's taking the address of the variable
-    s[0] = 't';
-    cout<<s<<endl;
+// void dosomething(string &s){ // now it's taking the address of the variable
+//     s[0] = 't';
+//     cout<<s<<endl;
+// }
+
+// int main(){
+//     string s = "suman";
+//     dosomething(s);
+//     cout<<s<<endl;
+//     return 0;
+// }
+
+
+// pass by reference
+void dosomething(int arr[], int n){ // ==>> array always goes with reference
+    arr[0] += 100;
+    cout << "value inside the function :" << arr[0] << endl;
 }
 
 int main(){
-    string s = "suman";
-    dosomething(s);
-    cout<<s<<endl;
+    int n = 5;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    dosomething(arr, n);
+
+    cout << arr[0] <<endl;
+
     return 0;
 }
