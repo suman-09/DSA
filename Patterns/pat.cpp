a20429263e94417d512ae56bfe8de0fb3d5e8a73
Suman Karmakar <sumankarmakar25112002@gmail.com>
2023-12-18 12:00:02 +0530

    pattern rule from striver

---
+++ Patterns/pat.cpp
@@ -0,0 +1,8 @@
+// two loops will be used in patterns
+// outer loops for the rows and inner loops for the columns
+
+// pathway ==>>
+// 1. for the outer loop count the number of rows
+// 2. for columns observe them and connect them somehow to the rows
+// 3. what ever you printing print the inner loop
+// 4. observe summetry (optional)
